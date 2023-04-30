8bd1e083d070f3b44f0f295f41491a372b14a0d1
Lifeisnoteasyauntyhelpyou <leo0932188@gmail.com>
2023-04-30 23:47:52 +0800

    version5-1

--- app/src/main/java/com/example/orderinssystem/SetMealA.java
+++ app/src/main/java/com/example/orderinssystem/SetMealA.java
@@ -3,11 +3,12 @@ package com.example.orderinssystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-public class ShoppingCar extends AppCompatActivity {
+
+public class SetMealA extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopping_car);
+        setContentView(R.layout.activity_set_meal_a);
     }
 }
